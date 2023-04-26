9121780fc22009cd680ad992a90d2f75ae96d350
Brian Arthur <tricpony@users.noreply.github.com>
2023-04-26 09:15:50 -0400

    Initial Commit

---
+++ AWRealEstate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AWRealEstate
+//
+//  Created by aarthur on 4/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
